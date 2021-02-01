c60727dc5f0b7967d6d5312de65718221acf7018
Tu Ha Dinh <hadinhtu97@gmail.com>
2021-02-01 14:14:13 +0700

    update

---
+++ pages/about/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import styles from '../../styles/main.module.css'
+
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+        </div>
+    )
+}
+
+export default About
--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,75 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/main.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>&lt; /&gt;</title>
+        <link rel="icon" href="/title.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Hello, I am <span className={styles.blue}>Tu.</span>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          A self-taught web developer.
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <Link href='/projects'>
+            <a className={styles.card}>
+              <h3 className={styles.blue}>Projects &rarr;</h3>
+              <p>Personal projects that I have completed or are developing.</p>
+            </a>
+          </Link>
+          <Link href='/skills'>
+            <a className={styles.card}>
+              <h3 className={styles.blue}>Skills &rarr;</h3>
+              <p>Programming languages, frameworks, library that I can use.</p>
+            </a>
+          </Link>
+          <Link href='/certifications'>
+            <a className={styles.card}>
+              <h3 className={styles.blue}>Certifications &rarr;</h3>
+              <p>All the certifications that I have completed.</p>
+            </a>
+          </Link>
+          <Link href='/blog'>
+            <a className={styles.card}>
+              <h3 className={styles.blue}>Blog &rarr;</h3>
+              <p>Articles about technology (in progress)</p>
+            </a>
+          </Link>
+          <Link href='/about'>
+            <a className={styles.card}>
+              <h3 className={styles.blue}>About &rarr;</h3>
+              <p>Brief description of myself.</p>
+            </a>
+          </Link>
+          <Link href='/contact'>
+            <a className={styles.card}>
+              <h3 className={styles.blue}>Contact &rarr;</h3>
+              <p>Contact with me through the following ways.</p>
+            </a>
+          </Link>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://nextjs.org/"
           target="_blank"
           rel="noopener noreferrer"
+          className={styles.blue}
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          I create this website by using {' '}
+          <img src="/nextjs.svg" alt="Nextjs Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
