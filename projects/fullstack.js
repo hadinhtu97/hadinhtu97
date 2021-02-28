e6f73b09e053ca38d82a7237dafb1ac1d7cb77b7
Tu Ha Dinh <hadinhtu97@gmail.com>
2021-02-28 23:29:16 +0700

    Chore: add backend projects data

---
+++ projects/fullstack.js
@@ -0,0 +1,12 @@
+export default {
+    type: 'Full Stack',
+    list: [
+        {
+            name: 'Weather App',
+            description: 'A web page that show weather info of current location.',
+            techs: ['React', 'Next'],
+            viewLink: 'https://weather-app.hadinhtu97.repl.co/',
+            souceLink: 'https://github.com/hadinhtu97/weather-app'
+        },
+    ]
+}
