3b36427b334e3c92d95e9fe51098da0ed89ce018
Tu Ha Dinh <hadinhtu97@gmail.com>
2021-03-01 18:33:42 +0700

    Feat: redirect Blogs page to Hashnote

--- pages/index.js
+++ pages/index.js
@@ -20,9 +20,9 @@ const Home = () => {
         <Article href='/projects' title='Projects' description='Personal projects that I have completed or are developing.' />
         <Article href='/skills' title='Skills' description='Languages, frameworks, libraries that I can use.' />
         <Article href='/certifications' title='Certifications' description='Certifications I have completed from online platforms.' />
-        <Article href='/blogs' title='Blogs' description='Some of my articles on technologies, etc (in progress).' />
         <Article href='/about' title='About' description='Briefly introduce myself and the work I have done.' />
         <Article href='/contact' title='Contact' description='Contact with me through the following ways.' />
+        <Article href='https://hadinhtu97.hashnode.dev/' title='Blogs' description='Some of my articles on technologies, etc (in progress).' />
       </section>
     </Container>
   )
