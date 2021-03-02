eb6e923f31c96c6722382be0f7787d1402af520f
Tu Ha Dinh <hadinhtu97@gmail.com>
2021-03-03 05:23:22 +0700

    add wikipedia-viewer project

--- data/fullstack.js
+++ data/fullstack.js
@@ -2,6 +2,13 @@ export default {
     type: 'Full Stack',
     list: [
         {
+            name: 'Wikipedia Viewer',
+            description: 'A web page that using Widipedia API to search articles.',
+            techs: ['React', 'Next'],
+            viewLink: 'https://wikipedia-viewer.hadinhtu97.repl.co/',
+            souceLink: 'https://github.com/hadinhtu97/wikipedia-viewer'
+        },
+        {
             name: 'Weather App',
             description: 'A web page that show weather info of current location.',
             techs: ['React', 'Next'],
