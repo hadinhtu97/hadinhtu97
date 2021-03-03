4c5ab919f3c80c72e032ffab13cfa4136260a59b
Tu Ha Dinh <hadinhtu97@gmail.com>
2021-03-04 00:14:53 +0700

    Add twitch api project

--- data/fullstack.js
+++ data/fullstack.js
@@ -2,15 +2,22 @@ export default {
     type: 'Full Stack',
     list: [
         {
+            name: 'Twitch API',
+            description: 'Using Twitch API to get infomation of channels.',
+            techs: ['React', 'Next'],
+            viewLink: 'https://twitch-api.hadinhtu97.repl.co/',
+            souceLink: 'https://github.com/hadinhtu97/twitch-api'
+        },
+        {
             name: 'Wikipedia Viewer',
-            description: 'A web page that using Widipedia API to search articles.',
+            description: 'Using Widipedia API to search articles.',
             techs: ['React', 'Next'],
             viewLink: 'https://wikipedia-viewer.hadinhtu97.repl.co/',
             souceLink: 'https://github.com/hadinhtu97/wikipedia-viewer'
         },
         {
             name: 'Weather App',
-            description: 'A web page that show weather info of current location.',
+            description: 'Show weather info of current location.',
             techs: ['React', 'Next'],
             viewLink: 'https://weather-app.hadinhtu97.repl.co/',
             souceLink: 'https://github.com/hadinhtu97/weather-app'
