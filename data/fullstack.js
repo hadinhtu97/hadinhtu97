7258c7822515339f2444a23a38fe9991c115a5af
Tu Ha Dinh <hadinhtu97@gmail.com>
2021-03-04 12:07:35 +0700

    Add image search project

--- data/fullstack.js
+++ data/fullstack.js
@@ -2,15 +2,22 @@ export default {
     type: 'Full Stack',
     list: [
         {
+            name: 'Image Search',
+            description: 'Using Google APIs to searching and receive images.',
+            techs: ['React', 'Next'],
+            viewLink: 'https://image-search.hadinhtu97.repl.co/',
+            souceLink: 'https://github.com/hadinhtu97/image-search'
+        },
+        {
             name: 'Twitch API',
-            description: 'Using Twitch API to get infomation of channels.',
+            description: 'Using Twitch APIs to get infomation of channels.',
             techs: ['React', 'Next'],
             viewLink: 'https://twitch-api.hadinhtu97.repl.co/',
             souceLink: 'https://github.com/hadinhtu97/twitch-api'
         },
         {
             name: 'Wikipedia Viewer',
-            description: 'Using Widipedia API to search articles.',
+            description: 'Using Widipedia APIs to search articles.',
             techs: ['React', 'Next'],
             viewLink: 'https://wikipedia-viewer.hadinhtu97.repl.co/',
             souceLink: 'https://github.com/hadinhtu97/wikipedia-viewer'
