ce9664dbe3700d30323b9f302fdaec4f77be8376
Tu Ha Dinh <hadinhtu97@gmail.com>
2021-07-05 14:46:46 +0700

    Restructure view of skills page

--- pages/skills.js
+++ pages/skills.js
@@ -26,31 +26,16 @@ const Skills = () => {
         <Container pageTitle='Skills'>
             <LinkToHome />
             <section className={styles.skills}>
-                <SkillGroup title='Frontend'>
+                <SkillGroup title='I would love to talk and work with'>
                     <Skill title='html' imgSrc='/skills/html.png' />
                     <Skill title='css' imgSrc='/skills/css.png' />
-                    <Skill title='sass' imgSrc='/skills/sass.png' />
-                    <Skill title='bootstrap' imgSrc='/skills/bootstrap.png' />
                     <Skill title='javascript' imgSrc='/skills/javascript.png' />
-                    <Skill title='ecma' imgSrc='/skills/ecmascript.png' />
-                    <Skill title='jquery' imgSrc='/skills/jquery.png' />
-                    <Skill title='d3' imgSrc='/skills/d3.jpeg' />
                     <Skill title='react' imgSrc='/skills/react.png' />
-                </SkillGroup>
-                <SkillGroup title='Backend'>
                     <Skill title='nodejs' imgSrc='/skills/nodejs.jpeg' />
-                    <Skill title='nextjs' imgSrc='/skills/nextjs.png' />
-                    <Skill title='express' imgSrc='/skills/express.jpeg' />
-                    <Skill title='chai' imgSrc='/skills/chai.jpeg' />
-                    <Skill title='mocha' imgSrc='/skills/mocha.png' />
-                    <Skill title='mongoose' imgSrc='/skills/mongoose.png' />
+                    <Skill title='express' imgSrc='/skills/express.jpeg' /> 
                     <Skill title='mongodb' imgSrc='/skills/mongodb.png' />
-                    <Skill title='sql' imgSrc='/skills/sql.jpeg' />
-                </SkillGroup>
-                <SkillGroup title='Programming'>
+                    <Skill title='nextjs' imgSrc='/skills/nextjs.png' />
                     <Skill title='python' imgSrc='/skills/python.jpeg' />
-                </SkillGroup>
-                <SkillGroup title='Others'>
                     <Skill title='git' imgSrc='/skills/git.png' />
                     <Skill title='docker' imgSrc='/skills/docker.png' />
                     <Skill title='linux' imgSrc='/skills/linux.jpeg' />
